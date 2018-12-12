46da11b4a5dd7827ee8571a438c64eaa59d64f4b
BenBoomy <43120024+BenBoomy@users.noreply.github.com>
2018-12-12 12:39:57 -0500

    Update Main.py
    everything is broken

--- Tetris/Main.py
+++ Tetris/Main.py
@@ -1,4 +1,5 @@
-#########################################
+########################################
+#
 # ##### ##### ##### ####   #####  ##### #
 #   #   #       #   #   #    #    #     #
 #   #   ###     #   ####     #    ##### #
@@ -33,69 +34,73 @@ CYAN = (0, 255, 255)
 
 ##### Classes and Functions #####
 
-class Wall(pg.sprite.Sprite):
+from Block import *
+from Wall import *
+from Bottom import *
 
-   def __init__(self, x):
-
-      pg.sprite.Sprite.__init__(self)
-      self.image = pg.Surface((100, 800))
-      self.image.fill(WHITE)
-
-      self.rect = self.image.get_rect()
-      self.rect.x = x
-      self.rect.y = 0
-
-      all_sprites.add(self)
-
-class Bottom(pg.sprite.Sprite):
-
-   def __init__(self, colour):
-
-       pg.sprite.Sprite.__init__(self)
-       self.image = pg.Surface((600, 100))
-       self.image.fill(colour)
-
-       self.rect = self.image.get_rect()
-       self.rect.x = 0
-       self.rect.y = 800
-
-       all_sprites.add(self)
-       bottom_sprites.add(self)
-
-class Block(pg.sprite.Sprite):
-
-    def __init__(self, x, y, colour):
-
-        pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((length, length))
-
-        self.image.fill(colour)
-
-        self.rect = self.image.get_rect()
-        self.rect.x = x + 100
-        self.rect.y = y
-
-        self.x = x
-        self.y = y
-        all_sprites.add(self)
-        player_sprites.add(self)
-
-    def left(self):
-        self.rect.x -= length
-        for sprite in player_sprites:
-           if sprite.rect.x < 100:
-              for sprite in player_sprites:
-                 sprite.rect.x += length
-                 
-    def right(self):
-        self.rect.x += length
-        for sprite in player_sprites:
-           if sprite.rect.x + length > scrn_width - 100:
-              for sprite in player_sprites:
-                 sprite.rect.x -= length
-
-    def down(self):
-        self.rect.y += length
+##class Wall(pg.sprite.Sprite):
+##
+##   def __init__(self, x):
+##
+##      pg.sprite.Sprite.__init__(self)
+##      self.image = pg.Surface((100, 800))
+##      self.image.fill(WHITE)
+##
+##      self.rect = self.image.get_rect()
+##      self.rect.x = x
+##      self.rect.y = 0
+##
+##      all_sprites.add(self)
+##
+##class Bottom(pg.sprite.Sprite):
+##
+##   def __init__(self, colour):
+##
+##       pg.sprite.Sprite.__init__(self)
+##       self.image = pg.Surface((600, 100))
+##       self.image.fill(colour)
+##
+##       self.rect = self.image.get_rect()
+##       self.rect.x = 0
+##       self.rect.y = 800
+##
+##       all_sprites.add(self)
+##       bottom_sprites.add(self)
+##
+##class Block(pg.sprite.Sprite):
+##
+##    def __init__(self, x, y, colour):
+##
+##        pg.sprite.Sprite.__init__(self)
+##        self.image = pg.Surface((length, length))
+##
+##        self.image.fill(colour)
+##
+##        self.rect = self.image.get_rect()
+##        self.rect.x = x + 100
+##        self.rect.y = y
+##
+##        self.x = x
+##        self.y = y
+##        all_sprites.add(self)
+##        player_sprites.add(self)
+##
+##    def left(self):
+##        self.rect.x -= length
+##        for sprite in player_sprites:
+##           if sprite.rect.x < 100:
+##              for sprite in player_sprites:
+##                 sprite.rect.x += length
+##                 
+##    def right(self):
+##        self.rect.x += length
+##        for sprite in player_sprites:
+##           if sprite.rect.x + length > scrn_width - 100:
+##              for sprite in player_sprites:
+##                 sprite.rect.x -= length
+##
+##    def down(self):
+##        self.rect.y += length
         
 def tetramino():
    new_piece = random.randint(0,6)
@@ -141,6 +146,9 @@ def tetramino():
       block_2 = Block(length, 0, CYAN)
       block_3 = Block(length, length, CYAN)
       block_4 = Block(2 * length, length, CYAN)
+
+   player_sprites.add(block_1, block_2, block_3, block_4)
+   all_sprites.add(block_1, block_2, block_3, block_4)
       
 def contact():
     if pg.sprite.groupcollide(player_sprites, bottom_sprites, False, False):
@@ -169,6 +177,7 @@ def rotate():
          x2 = sprite.rect.x
          
       if sprite.rect.y < y1:
+         all_sprites.add(self)
          y1 = sprite.rect.y
       if sprite.rect.y > y2:
          y2 = sprite.rect.y
@@ -197,8 +206,10 @@ pg.display.init()
 
 left_wall = Wall(0)
 right_wall = Wall(500)
+bottom = Bottom(WHITE)
+bottom_sprites.add(bottom)
+all_sprites.add(left_wall, right_wall, bottom)
 
-bottom_sprite = Bottom(WHITE)
 tetramino()
 
 while gameLoop:
