61c73ec65b2024ad0c74689992a483f585603176
BenBoomy <43120024+BenBoomy@users.noreply.github.com>
2018-12-18 14:01:21 -0500

    Update Block.py

--- Tetris/Block.py
+++ Tetris/Block.py
@@ -4,32 +4,28 @@ class Block(pg.sprite.Sprite):
 
     def __init__(self, x, y, colour):
 
-        self.length = 20
+        self.length = 25
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((self.length, self.length))
 
         self.image.fill(colour)
 
         self.rect = self.image.get_rect()
-        self.rect.x = x + 100
-        self.rect.y = y
+        self.rect.x = int(x) * self.length + 50
+        self.rect.y = int(y) * self.length + 50
 
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
+        print self.x, self.y
 
     def left(self):
         self.rect.x -= self.length
-        for sprite in player_sprites:
-           if sprite.rect.x < 100:
-              for sprite in player_sprites:
-                 sprite.rect.x += self.length
-                 
+
     def right(self):
         self.rect.x += self.length
-        for sprite in player_sprites:
-           if sprite.rect.x + self.length > scrn_width - 100:
-              for sprite in player_sprites:
-                 sprite.rect.x -= self.length
 
     def down(self):
         self.rect.y += self.length
+        
+        
+    
