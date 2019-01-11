4cc9802025af642b79159e9251eecff0b63aca72
BenBoomy <43120024+BenBoomy@users.noreply.github.com>
2019-01-11 14:08:56 -0500

    Update Main.py

--- Tetris/Main.py
+++ Tetris/Main.py
@@ -11,6 +11,8 @@
 
 import pygame
 import random
+from threading import Timer
+import time
 
 from Block import Block
 from Wall import Wall
@@ -42,112 +44,130 @@ def list2matrix():
 
     for i in player_blocks:
         matrix[i.y][i.x] = i
+    return matrix
 
-    for row in matrix:
-        print row
+def createPiece(colour, pos1, pos2, pos3, pos4):
+    block_1 = Block(pos1[0], pos1[1], colour)
+    block_2 = Block(pos2[0], pos2[1], colour)
+    block_3 = Block(pos3[0], pos3[1], colour)
+    block_4 = Block(pos4[0], pos4[1], colour)
+    
+    player_objects.add(block_1, block_2, block_3, block_4)
+    all_objects.add(block_1, block_2, block_3, block_4)
+    
+    player_blocks.append(block_1)
+    player_blocks.append(block_2)
+    player_blocks.append(block_3)
+    player_blocks.append(block_4)
+
+def down():
+    for block in player_blocks:
+        block.down()
+
+def turnPiece(matrix, direction):
+    global idx
+    if direction:
+        if idx == 0: #means piece is in right position
+            new = zip(matrix[0], matrix[1])
+            for row in matrix:
+                for item in row:
+                    try:
+                        print item.rect.y
+                    except AttributeError:
+                        pass
+            print matrix
+##            for i in matrix:
+##                for j in i:
+##                    try:
+##                        print matrix[i][j]
+##                    except:
+##                        print "wassup"
+            corner = matrix[0][0]
+        elif idx == 2: #means piece is in left position
+            corner = matrix[0][3]
+        elif idx  == 1: #means piece is in top position
+            corner = matrix[0][0]
+        elif idx == 3: #means piece is in bottom position
+            corner = matrix[0][1]
+
+        if idx == 3:
+            idx = 0
+        else:
+            idx += 1
+
+    if not direction:
+        if idx == 0: #means piece is in right position
+            corner = matrix[0][0]
+        elif idx == 2: #means piece is in left position
+            corner = matrix[0][3]
+        elif idx  == 1: #means piece is in top position
+            corner = matrix[0][0]
+        elif idx == 3: #means piece is in bottom position
+            corner = matrix[0][1]
+
+        if idx == 1:
+            idx = 0
+            
+        else:
+            idx -= 1
 
 def newPiece():
     num = random.randint(1, 7)
-    
-    if num == 1:
-        block_1 = Block(0, 0, RED)
-        block_2 = Block(1, 0, RED)
-        block_3 = Block(2, 0, RED)
-        block_4 = Block(3, 0, RED)
-        player_objects.add(block_1, block_2, block_3, block_4)
-        all_objects.add(block_1, block_2, block_3, block_4)
-        player_blocks.append(block_1)
-        player_blocks.append(block_2)
-        player_blocks.append(block_3)
-        player_blocks.append(block_4)
-        
-    elif num == 2:
-        block_1 = Block(0, 0, ORANGE)
-        block_2 = Block(1, 0, ORANGE)
-        block_3 = Block(2, 0, ORANGE)
-        block_4 = Block(2, 1, ORANGE)
-        player_objects.add(block_1, block_2, block_3, block_4)
-        all_objects.add(block_1, block_2, block_3, block_4)
-        player_blocks.append(block_1)
-        player_blocks.append(block_2)
-        player_blocks.append(block_3)
-        player_blocks.append(block_4)
 
+    if num == 1:
+        createPiece(RED, (0,0), (1,0), (2,0), (3,0))
+    elif num == 2:
+        createPiece(ORANGE, (0,0), (1,0), (2,0), (2,1))
     elif num == 3:
-        block_1 = Block(0, 0, MAGENTA)
-        block_2 = Block(0, 1, MAGENTA)
-        block_3 = Block(1, 0, MAGENTA)
-        block_4 = Block(2, 0, MAGENTA)
-        player_objects.add(block_1, block_2, block_3, block_4)
-        all_objects.add(block_1, block_2, block_3, block_4)
-        player_blocks.append(block_1)
-        player_blocks.append(block_2)
-        player_blocks.append(block_3)
-        player_blocks.append(block_4)
-        
+        createPiece(MAGENTA, (0,0), (0,1), (1,0), (2,0))
     elif num == 4:
-        block_1 = Block(0, 0, BLUE)
-        block_2 = Block(0, 1, BLUE)
-        block_3 = Block(1, 0, BLUE)
-        block_4 = Block(1, 1, BLUE)
-        player_objects.add(block_1, block_2, block_3, block_4)
-        all_objects.add(block_1, block_2, block_3, block_4)
-        player_blocks.append(block_1)
-        player_blocks.append(block_2)
-        player_blocks.append(block_3)
-        player_blocks.append(block_4)
-        
+        createPiece(BLUE, (0,0), (0,1), (1,0), (1,1))
     elif num == 5:
-        block_1 = Block(0, 1, GREEN)
-        block_2 = Block(1, 0, GREEN)
-        block_3 = Block(1, 1, GREEN)
-        block_4 = Block(2, 0, GREEN)
-        player_objects.add(block_1, block_2, block_3, block_4)
-        all_objects.add(block_1, block_2, block_3, block_4)
-        player_blocks.append(block_1)
-        player_blocks.append(block_2)
-        player_blocks.append(block_3)
-        player_blocks.append(block_4)
-        
+        createPiece(GREEN, (0,1), (1,0), (1,1), (2,0))
     elif num == 6:
-        block_1 = Block(0, 0, OLIVE)
-        block_2 = Block(1, 0, OLIVE)
-        block_3 = Block(1, 1, OLIVE)
-        block_4 = Block(2, 0, OLIVE)
-        player_objects.add(block_1, block_2, block_3, block_4)
-        all_objects.add(block_1, block_2, block_3, block_4)
-        player_blocks.append(block_1)
-        player_blocks.append(block_2)
-        player_blocks.append(block_3)
-        player_blocks.append(block_4)
-        
+        createPiece(OLIVE, (0,0), (1,0), (1,1), (2,0))
     elif num == 7:
-        block_1 = Block(0, 0, CYAN)
-        block_2 = Block(1, 0, CYAN)
-        block_3 = Block(1, 1, CYAN)
-        block_4 = Block(2, 1, CYAN)
-        player_objects.add(block_1, block_2, block_3, block_4)
-        all_objects.add(block_1, block_2, block_3, block_4)
-        player_blocks.append(block_1)
-        player_blocks.append(block_2)
-        player_blocks.append(block_3)
-        player_blocks.append(block_4)
-        
+        createPiece(CYAN, (0,0), (1,0), (1,1), (2,1))
+
 def main():
     pygame.init()
     pygame.display.init()
-    screen = pygame.display.set_mode((550, 900))
+    screen = pygame.display.set_mode((600, 1000))
     gameLoop = True
+    t1 = 0
+    
+    global idx
+    idx = 0
 
-    right = Wall(500)
-    left = Wall(0)
-    all_objects.add(right)
-    all_objects.add(left)
-    newPiece()
-    list2matrix()
+    right_wall = Wall(550)
+    left_wall = Wall(0)
+    all_objects.add(right_wall)
+    all_objects.add(left_wall)
+    
+##    newPiece()
+##    matrix = list2matrix()
+
     while gameLoop:
+        if len(player_blocks) == 0:
+            newPiece()
+            matrix = list2matrix()
+            
+        for block in player_objects:
+            if block.rect.y >= 875:
+                player_objects.empty()
+                bottom_objects.add(block)
+                player_blocks[:] = []
+                
+        #moving piece down automatically
+        t = time.time()
+        if t - t1 > 1:
+            down()
+            t1 = t
+            
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
+                #movement management
+                
                 if event.key == pygame.K_LEFT:
                     for block in player_blocks:
                         block.left()
@@ -155,18 +175,23 @@ def main():
                             if block.rect.x < 50:
                                 for block in player_blocks:
                                     block.right()
+                                    
                 elif event.key == pygame.K_RIGHT:
-                    print player_blocks
                     for block in player_blocks:
                         block.right()
                         for block in player_blocks:
-                            if block.rect.x >= 500:
+                            if block.rect.x >= 550:
                                 for block in player_blocks:
                                     block.left()
-                elif event.key == pygame.K_DOWN:
-                    for block in player_blocks:
-                        block.down()
+                elif event.key == pygame.K_UP:
+                    turnPiece(matrix, True)
 
+                elif event.key == pygame.K_DOWN:
+                    turnPiece(matrix, False)
+                    
+                if event.key == pygame.K_SPACE:
+                    down()
+        
         all_objects.update()
         screen.fill(BLACK)
         all_objects.draw(screen)
